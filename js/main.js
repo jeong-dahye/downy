4ccbe928344c466ec339c759ad4c9d1fc519bf0c
jeongdh <77033243+jeong-dahye@users.noreply.github.com>
2021-02-04 14:19:41 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function(){
+     $('.').slick({
+        slidesToShow:1, //보이는 슬라디드
+        slidesToScroll:1, //슬라이드 넘기는 수
+        centerMode:false,//이미지를 가운데 정렬하여 잘린 이미지도 보이도록 해주는 역할
+        arrows:false, //화살표
+        prevArrow:$('.btnL'), //화살표 클래스 설정
+		nextArrow:$('.btnR'),
+        dots:false,
+        dotsClass:'dotBtn',
+        variableWidth:true,
+        autoplay:true,//자동재생
+        autoplaySpeed:2500,//재생속도
+        infinite:true, //반복재생
+        });
+
+});*/
+
+
+/*alert('오류')
+document.getElementById('wrap').style.display="none"*/
